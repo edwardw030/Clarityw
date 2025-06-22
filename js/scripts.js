b958d779da8aef84b962ef13f5c55e5e0a0230a5
Edward Wang <edwardwang030@gmail.com>
2025-06-22 13:43:58 -0700

    added pokemon list to js
    created a list of pokemons with traits

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World!');
-
-let myfavourtieFood= 'Pizza';
-document.writeln(myfavourtieFood);
+let pokemonList = [
+    { name: 'bulbsaur', height: '0.7', types: ['grass', 'poision'], weight: '6.9'},
+    { name: 'charmander', height:'0.6', types: 'fire', weight:'8.5'},
+    { name: 'squirtle', height: '0.5', types: 'water', weight: '9'},
+    { name: 'pikachu', height: '0.4', types: 'electric', weight: '6'},
+    { name: 'eevee', height: '0.3', types: 'normal', weight: '6.5'}
+]
