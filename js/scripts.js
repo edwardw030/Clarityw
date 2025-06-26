1c71a1793f3f5ff968c7457fc97730911118410b
Edward Wang <edwardwang030@gmail.com>
2025-06-26 11:20:22 -0700

    Changes to scripts.js file
    added a for loop to iterate each item with height condition

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,17 @@ let pokemonList = [
     { name: 'squirtle', height: '0.5', types: 'water', weight: '9'},
     { name: 'pikachu', height: '0.4', types: 'electric', weight: '6'},
     { name: 'eevee', height: '0.3', types: 'normal', weight: '6.5'}
-]
+];
+
+// for loop that iterates over each pokemon with height next to it with the condtion of height message
+for (let i = 0; i < pokemonList.length; i++) {
+    if(pokemonList[i].height > 0.6) {
+        message = '-This is a big pokemon!';
+    } else {
+        message = '';
+    }
+    document.writeln (
+        `${pokemonList[i].name} - (height: ${pokemonList[i].height}) ${message} <br> `
+    );
+
+}
